db3bd56a6bcb527e87a316b67063d42669073ec0
Aiden Wahed <prismhead26@gmail.com>
2024-05-10 16:01:42 -0600

    Initialization

---
+++ server/config/connection.js
@@ -0,0 +1,8 @@
+// requires mongoose and connects to the mongo database
+const mongoose = require('mongoose');
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+
+// Export the connection
+module.exports = mongoose.connection;
