11abddd74b4fb42d8ddec0334c1acb30cf5c5f46
Aiden Wahed <prismhead26@gmail.com>
2024-05-17 00:26:30 -0600

    server config

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 
 // Connect to the Mongo DB
 mongoose.connect(
+  // process.env.MONGODB_URI || "mongodb://localhost/googlebooks"
   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/googlebooks"
 );
 
